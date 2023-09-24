2da0207305f6f8ca3b2ad6716c836d8118ac8d9a
Yifei-code <yz7901@g.rit.edu>
2023-09-25 05:39:57 +0800

    sub

--- calculator.py
+++ calculator.py
@@ -3,5 +3,10 @@ def add(x,y):
     result = x + y
     return f"{x} + {y} = {result}"
 
+def subtract(x,y):
+    result = x - y
+    return f"{x} - {y} = {result}"
+
 def main():
     print(add())
+    print(subtract())
--- calculator_test.py
+++ calculator_test.py
@@ -5,5 +5,11 @@ def test_add():
     assert calculator.add(5,7) == "5 + 7 = 12"
     assert calculator.add(100,234) == "100 + 234 = 334"
     
+def test_sub():
+    assert calculator.subtract(6,1) == "6 - 1 = 5"
+    assert calculator.subtract(2,17) == "2 - 17 = -15"
+    
+    
 if __name__ == "__main__":
     test_add()
+    test_sub()
